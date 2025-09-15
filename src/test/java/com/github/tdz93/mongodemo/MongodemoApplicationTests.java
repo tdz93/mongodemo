f267ce7dbf9439a68c4c77ecf9e483bdc9a4593d
Tharuka De Zoysa <tharuka.dezoysa@sourcesense.com>
2025-09-15 16:03:57 +0200

    chore: inizializzazione progetto

---
+++ src/test/java/com/github/tdz93/mongodemo/MongodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tdz93.mongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
